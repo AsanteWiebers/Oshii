09c94fc7ac93704bb4e9345f2964d70794cc4375
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-05-23 21:18:21 +0200

    multiple file changes

---
+++ main.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.info_header', {
+    delay: 150,
+    origin: 'top'
+});
+
+
